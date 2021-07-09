523ee3f09977946971212e59581f744c36ae0a79
TanveshRongala <tanveshrongala@gmail.com>
2021-07-09 22:50:59 +0530

    first commit

---
+++ src/SnakeAndLadder.java
@@ -0,0 +1,40 @@
+
+public class SnakeAndLadder {
+	public static void main(String[] args)
+	{
+		int count = 0;
+	    int position = 0;
+	    while(position<=99){
+	    	System.out.println("position is " + position);
+			int dice=(int) (Math.random() * 10) % 6+1;
+			position = (position + dice);
+			count =(count + 1);
+			System.out.println("dice value is " + dice);
+			int check = (int) (Math.random() * 10) % 3+1;
+			switch(check) {
+			   case 1:
+			   System.out.println( "No Play You are in the same position" );
+			   break;
+			  
+			   case 2:
+			   position = (position + dice);
+			   if(position>100){
+				   position = (position - dice);
+				   continue;
+			   }
+			   System.out.println( "your position after ladder is :" + position );
+			   break;
+			   
+			   case 3: 
+			   position = (position - dice);
+			  
+			  if(position<0){
+				  position = 0;
+			  }
+				 System.out.println( "Your Position after snake is" + position );
+			   break;
+			  }
+			 }
+			System.out.println( "Dice play number " + count );
+	}
+}
